2d4a10d9337fd052a47ebd21740a8fc60171cd34
Shubham Sinha <sinhashubham371@gmail.com>
2024-09-01 23:28:29 +0530

    Added common entity service

---
+++ src/main/java/org/example/uberprojectentityservice/models/BookingStatus.java
@@ -0,0 +1,12 @@
+package org.example.uberprojectentityservice.models;
+
+public enum BookingStatus {
+
+    SCHEDULED,
+    CANCELLED,
+    CAB_ARRIVED,
+    ASSIGNING_DRIVER,
+    IN_RIDE,
+    COMPLETED
+
+}
---
+++ src/main/java/org/example/uberprojectentityservice/models/Driver.java
@@ -0,0 +1,34 @@
+package org.example.uberprojectentityservice.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","bookings"}) //if not used then driver->booking->driver->booking...so on (will throw nested recursive response all together)
+public class Driver extends BaseModel {
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String licenseNumber;
+
+    private String phoneNumber;
+
+    @OneToMany(mappedBy = "driver", fetch = FetchType.LAZY)
+    @Fetch(FetchMode.SUBSELECT) // to solve N+1 problem
+    private List<Booking> bookings;
+
+}
